7ccf1adeb053f75be39f46ca8df10ff4520fcb56
rtalking10 <98490662+rtalking10@users.noreply.github.com>
2022-02-13 14:41:36 -0600

    Update RetrievalCode.py

--- RetrievalCode.py
+++ RetrievalCode.py
@@ -5,9 +5,3 @@ LOCAL_FILE = 'local_copy.log'
 
 # Use urlretrieve() to fetch a remote copy and save into the local file path
 local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
-
-# Alt.: supply an anonmymous callback function to print a simple progress bar to screen
-local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE, lambda x,y,z: print('.', end='', flush=True))
-
-# Alt. 2: a progress bar with reduced output (every 1000 blocks)
-local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE, lambda x,y,z: print('.', end='', flush=True) if x % 100 == 0 else False)
