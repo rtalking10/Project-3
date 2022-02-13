86b51754fab2b353f54c7d0c1edc03f4621a2657
rtalking10 <98490662+rtalking10@users.noreply.github.com>
2022-02-13 14:38:51 -0600

    Update RetrievalCode.py

--- RetrievalCode.py
+++ RetrievalCode.py
@@ -1,1 +1,13 @@
-This is where the retrieval code will be written
+from urllib.request import urlretrieve
+
+URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+LOCAL_FILE = 'local_copy.log'
+
+# Use urlretrieve() to fetch a remote copy and save into the local file path
+local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
+
+# Alt.: supply an anonmymous callback function to print a simple progress bar to screen
+local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE, lambda x,y,z: print('.', end='', flush=True))
+
+# Alt. 2: a progress bar with reduced output (every 1000 blocks)
+local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE, lambda x,y,z: print('.', end='', flush=True) if x % 100 == 0 else False)
