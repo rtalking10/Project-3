3f27173939876fb82246d3f9b4117efca901edbc
rtalking10 <98490662+rtalking10@users.noreply.github.com>
2022-02-15 21:15:50 -0600

    Update MktgRequest.py

--- MktgRequest.py
+++ MktgRequest.py
@@ -14,15 +14,31 @@ if not exist('local_copy.log'):
 FILE_NAME = 'local_copy.log'
 
 with open(FILE_NAME) as f:
-    lines = [line.rstrip('\n') for line in f]
+    requests = [line.rstrip('\n') for line in f]
 
 #returns the number of lines in lines;
 #it is also the nubmer of requests in the life of the log
-length = len(lines)
+length= len(requests)
 
-#Will return the number of requests in the last 6 months
-numreq = lines.count("Feb/2022") + lines.count("Jan/2022") + lines.count("Dec/2021") + lines.count("Nov/2021") + lines.count("Oct/2021") + lines.count("Sep/2021")
+#Should return the number of requests in the last 6 months
+months = 0
+i = 0
+
+while i < length:
+    if "Oct/1995" in requests[i]:
+        months += 1
+    elif "Sep/1995" in requests[i]:
+        months += 1
+    elif "Aug/1995" in requests[i]:
+        months += 1
+    elif "Jul/1995" in requests[i]:
+        months += 1
+    elif "Jun/1995" in requests[i]:
+        months += 1
+    elif "May/1995" in requests[i]:
+        months += 1
+    i += 1
 
 #Prints out the information for the marketing request
-print("Here are the number of requests to the server in the last 6 months: " + str(numreq))
+print("Here are the number of requests to the server in the last 6 months: " + str(months))
 print("Here are the number of requests in the life of the log: " + str(length))
